fb26c3abea2c60265ff2e91b103e90d94e57ec7f
Akul Santhosh <akul753@gmail.com>
2019-06-12 02:19:32 -0700

    Add files via upload

--- new/js/firebase.js
+++ new/js/firebase.js
@@ -1,13 +1,70 @@
 var config = {
-    apiKey: "AIzaSyCN7Bw8pL0mtodqUNTNLqA1NzS0VsIvTR8",
-    authDomain: "semantic-b4ee2.firebaseapp.com",
-    projectId: "semantic-b4ee2",
-    storageBucket: "semantic-b4ee2.appspot.com",
-    messagingSenderId: "422426875104",
-    appId: "1:422426875104:web:243d9278547b4e89"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyCN7Bw8pL0mtodqUNTNLqA1NzS0VsIvTR8",
+  authDomain: "semantic-b4ee2.firebaseapp.com",
+  projectId: "semantic-b4ee2",
+  storageBucket: "semantic-b4ee2.appspot.com",
+  messagingSenderId: "422426875104",
+  appId: "1:422426875104:web:243d9278547b4e89"
+};
+firebase.initializeApp(config);
 
-  
+
 var db = firebase.firestore();
+lst = [];
 
+location_data = [];
+function data_capture() {
+  var ref = db.collection("semantic labs");
+  ref.onSnapshot((snapshot) => {
+    snapshot.forEach((snap) => {
+      location_dict = {}
+      location_data_each = snap.data();
+      subtitle = {}
+      for(var edge in location_data_each["Connected Edges"]){
+        subtitle_each = location_data_each["Connected Edges"][edge].Name
+        list_of_card = []
+        for(var device in location_data_each["Connected Edges"][edge]["Connected Devices"]){
+            heading =  location_data_each["Connected Edges"][edge]["Connected Devices"][device].Name
+            body = location_data_each["Connected Edges"][edge]["Connected Devices"][device].data.current
+
+            let each_card_dict = {
+                heading:heading,
+                body:body,
+                "class":"card-expand",
+
+            }
+            list_of_card.push(each_card_dict)
+          }
+          subtitle[subtitle_each] = list_of_card
+      }
+      location_dict.title = location_data_each.Name;
+      location_dict.subtitles = subtitle
+      location_data.push(location_dict);
+    })
+    create_container(location_data)
+    // console.log(location_data)
+  })
+}
+data_capture()
+
+var i = {
+  "title": "Semantic Lab (Kochi)",
+  "subtitles": {
+      "Rasberry Pi 3 (1)": [
+          {
+              "heading": "<h1 class='title_text'>Temp Sensor</h1>",
+              "body":"<h1>23°</h1>",
+              "footer": "",
+              "class": "card-expand",
+              "more_info":"Hello There"
+          },
+          {
+              "heading": "<h1 class='title_text'> Hum Sensor</h1>",
+              "body": "<h1>77%</h1>",
+              "footer": ""
+          }
+      ],
+      "Rasberry Pi 3 (2)": [
+      ]
+  }
+}
--- new/js/functionality.js
+++ new/js/functionality.js
@@ -1,43 +1,74 @@
 
-function create_card(info) {
+function create_card(card_information) {
+
+    heading = card_information.heading
+    body = card_information.body
+    footer = card_information.footer
+    card_class = card_information.class
+    card_side_info = card_information.more_info
+    side_col = ""
+    footer_html=""
+    if (footer) {
+        footer_html = `<div class="card-footer">`+ footer + `</div>`
+    }
+    if (card_side_info) {
+        side_col = `<div class="col">
+                                `+ card_side_info + `
+                    </div>`
+
+    }
     let card = `<div class="col">
-        <div class="card card-1">
-        <div class="card-header">`+info+`</div>
-        <div class="card-body"></div> 
-        <div class="card-footer">Footer</div>
-        </div>
-    </div>`
+                    <div class="card card-1 `+ card_class + `">
+                        <div class = "row">
+                            <div class = "col">
+                                <div class="card-header">`+ heading + `</div>
+                                <div class="card-body">`+ body + `</div> 
+                                `+footer_html+`
+                            </div>
+                            `+side_col+`
+                        </div>
+                     </div>
+                </div>
+                `
 
     return card
 }
 
-function create_row(title, list_of_cards) {
+function create_row(location_data) { //
 
-    let cols = []
-    for (var i = 0; i < list_of_cards.length; i++) {
-        a = create_card(list_of_cards[i])
-        cols.push(a)
-    }
+    title = location_data.title
+    subtitles_with_card = location_data.subtitles
+    // cards = location_data.cards
 
-    text_value = cols.join(" ")
-
-    row = `<div class="row mt-5">
+    total_html = `<div class="row mt-5">
                 <div class="col">
                     <h3>`+ title + `</h3>
                 </div>
-                </div>
-                <div class="row row-fluid">
-                    `+text_value+`
-                </div>
             </div>`
-    return row
+
+
+    for (var subtitle in subtitles_with_card) {
+        let cols = []
+        for (var i = 0; i < subtitles_with_card[subtitle].length; i++) {
+            a = create_card(subtitles_with_card[subtitle][i])
+            cols.push(a)
+        }
+        text_value = cols.join(" ")
+        row_html = `<h5>` + subtitle + `</h5 >
+            <div class="row row-fluid">
+                `+ text_value + `
+            </div>`
+        total_html += row_html
+    };
+
+
+    return total_html
 }
 
-function create_container(list_of_locations){
+function create_container(list_of_locations) {
     let cols = []
     for (var i = 0; i < list_of_locations.length; i++) {
-        a = create_row(list_of_locations[i],["sahil","akul","rajath"])
-        console.log(a)
+        a = create_row(list_of_locations[i])
         cols.push(a)
     }
 
@@ -47,4 +78,30 @@ function create_container(list_of_locations){
 
 }
 
-create_container(["Semantic Lab (Kochi)","Semantic Lab (Banglore)"])
+create_container([
+    {
+        "title": "Semantic Lab (Kochi)",
+        "subtitles": {
+            "Rasberry Pi 3 (1)": [
+                {
+                    "heading": "<h1 class='title_text'>Temp Sensor</h1>",
+                    "body":"<h1>23°</h1>",
+                    "footer": "",
+                    "class": "card-expand",
+                    "more_info":"Hello There"
+                },
+                {
+                    "heading": "<h1 class='title_text'> Hum Sensor</h1>",
+                    "body": "<h1>77%</h1>",
+                    "footer": ""
+                }
+            ],
+            "Rasberry Pi 3 (2)": [
+            ]
+        }
+    },
+    {
+        "title": "Semantic Lab (Banglore)",
+        "subtitles": {
+        }
+    }])
